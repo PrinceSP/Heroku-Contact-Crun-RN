ed3b2fa8fb0c7a082467fae713491e3114417e69
PrinceSP <princepasombaran@gmail.com>
2024-04-04 18:57:05 +0700

    feat: create separate component for AllContacts datas

---
+++ components/modules/AllContacts/index.jsx
@@ -0,0 +1,29 @@
+import React,{memo} from 'react'
+import {View,Text,StyleSheet,Dimensions,FlatList,Image,Pressable} from 'react-native'
+import { AntDesign,Feather } from '@expo/vector-icons';
+
+const {fontScale} = Dimensions.get('window')
+
+const AllContacts = memo(({item} ) => (
+  <Pressable
+    onPress={() => console.log(item.id)}
+    style={{
+      borderBottomWidth: 1,
+      borderBottomColor: '#eee',
+      paddingBottom: 10,
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    }}>
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Image source={{ uri: item.photo }} style={{ height: 50, width: 50, borderRadius: 14 }} />
+      <Text style={{ marginLeft: 20, color: '#444', fontSize: 18 / fontScale }}>{item.firstName + ' ' + item.lastName}</Text>
+    </View>
+    <View style={{ backgroundColor: '#AED6F1', padding: 5, borderRadius: 5 }}>
+      <AntDesign name="right" size={16} color="#fff" />
+    </View>
+  </Pressable>
+));
+
+export default AllContacts
