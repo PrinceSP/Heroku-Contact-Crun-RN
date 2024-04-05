9a82e2b28354f79250838317997cb898946a413a
PrinceSP <princepasombaran@gmail.com>
2024-04-05 13:49:48 +0700

    style: set status bar style to auto

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {Provider} from 'react-redux'
 import {store} from './store'
 import Router from './router'
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
+        <StatusBar style="auto" />
         <Router/>
-        <StatusBar backgroundColor="#fff" barStyle="dark-content"/>
       </NavigationContainer>
     </Provider>
   );
