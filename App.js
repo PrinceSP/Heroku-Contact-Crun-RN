c9ae0efa98111aba1fbc9f8d1704dc994a051b1f
PrinceSP <princepasombaran@gmail.com>
2024-04-05 12:14:38 +0700

    feat: add redux provider wrapper

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import {Provider} from 'react-redux'
+import {store} from './store'
 import Router from './router'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router/>
-      <StatusBar backgroundColor="#fff" barStyle="dark-content"/>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router/>
+        <StatusBar backgroundColor="#fff" barStyle="dark-content"/>
+      </NavigationContainer>
+    </Provider>
   );
 }
