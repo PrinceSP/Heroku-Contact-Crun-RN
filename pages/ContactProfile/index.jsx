ebc38934e1b142eee03414e2d41f5bbeeae6df6e
PrinceSP <princepasombaran@gmail.com>
2024-04-05 13:54:40 +0700

    feat: add value from redux store, add selected contact data

--- pages/ContactProfile/index.jsx
+++ pages/ContactProfile/index.jsx
@@ -1,34 +1,47 @@
 import React,{memo} from 'react'
-import {View,Text,StyleSheet,SafeAreaView,Dimensions,TextInput,FlatList,Image,Pressable} from 'react-native'
+import {View,Text,StyleSheet,SafeAreaView,Dimensions,TextInput,FlatList,Image,ImageBackground,Pressable} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSelector } from 'react-redux';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Feather } from '@expo/vector-icons';
 import {useGetData} from '../../custom-hooks'
 import {BackButton} from '../../components'
-import {MaterialCommunityIcons} from "@expo/vector-icons"
-import { useSelector } from 'react-redux';
 
-const {width,fontScale} = Dimensions.get('screen')
+const {width,fontScale} = Dimensions.get('window')
 
 const ContactProfile = ({navigation}) => {
   const insets = useSafeAreaInsets()
   const id = useSelector(state => state.currentID.currentId);
-  const {firstName:first,lastName:last,age,photo } = useGetData(`${process.env.BASE_URL}/${id}`)?.data
-  console.log(first);
+  const selectedContact = useGetData(`${process.env.BASE_URL}/${id}`)
+  const {firstName:first,lastName:last,age,photo } = Object(selectedContact?.data)
 
   return (
-    <SafeAreaView style={[styles.container,{paddingTop: insets.top}]}>
-      <View style={styles.header}>
-        <BackButton onPress={()=>navigation.goBack()}/>
-        <MaterialCommunityIcons name="pencil-outline" size={18} color="#777" />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <ImageBackground blurRadius={3} style={{flex:1,paddingHorizontal:21,alignItems:'center'}} source={{uri:photo}}>
+        <View style={[styles.header,{marginTop:insets.top}]}>
+          <BackButton onPress={()=>navigation.goBack()} backgroundColor="#fff" color="#777" padding={8}/>
+        </View>
+        <View style={styles.contentContainer}>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: photo }} />
+          </View>
+          <Text style={styles.name}>{`${first} ${last}` || "-"}</Text>
+          <Pressable style={{backgroundColor:"#aea3a5",borderRadius:50,paddingVertical:10,paddingHorizontal:5}}>
+            <Feather name="edit-3" size={24} color="black" />
+            <Text>Edit Contact</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    height:"100%",
-    paddingHorizontal:21,
-    backgroundColor:"#fff"
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   title: {
     alignSelf: 'center',
@@ -36,6 +49,40 @@ const styles = StyleSheet.create({
     fontSize:26/fontScale,
     marginTop:25,
     fontWeight:'700'
+  },
+  header:{
+    width,
+    paddingHorizontal:21
+  },
+  contentContainer:{
+    width,
+    flex:1,
+    backgroundColor:"#fff",
+    alignItems:'center',
+    marginTop:130,
+    borderTopRightRadius:40,
+    borderTopLeftRadius:40
+  },
+  imageContainer:{
+    width:130,
+    height:130,
+    borderRadius:34,
+    borderWidth:3,
+    borderColor:"#f56e53",
+    position:'absolute',
+    top:-70,
+    padding:2
+  },
+  image:{
+    width: "100%",
+    height: "100%",
+    borderRadius: 30
+  },
+  name:{
+    fontSize:24/fontScale,
+    fontWeight:'800',
+    color:"#454545",
+    marginTop:90
   }
 });
 
