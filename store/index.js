0a19c1e40782ed26ff537f11ca574bc6a64f2f6a
PrinceSP <princepasombaran@gmail.com>
2024-04-06 19:12:37 +0700

    feat: create reducer for selected contact

--- store/index.js
+++ store/index.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import contactId from './contactId.js'
+import selectedContact from './selectedContact.js'
 
 export const store = configureStore({
   reducer:{
     currentID: contactId,
+    contactData: selectedContact,
   }
 })
