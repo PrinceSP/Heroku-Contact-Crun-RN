48e1625c57fedf6c716b9a893ed46277017d76dc
PrinceSP <princepasombaran@gmail.com>
2024-04-06 22:26:23 +0700

    feat: add button to navigate to selec current contact

--- components/modules/RecentAdd/index.jsx
+++ components/modules/RecentAdd/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import {View,Text,StyleSheet,Dimensions,FlatList,Image,Pressable} from 'react-native'
+import { useDispatch } from 'react-redux';
+import { getID } from '../../../store/contactId'; // Import the action creator
 
 const {fontScale} = Dimensions.get('screen')
 
-const RecentAdd = ({datas}) => {
+const RecentAdd = ({datas,navigation}) => {
+  const dispatch = useDispatch();
+
+  const getCurrentContact = (item) => {
+    dispatch(getID({currentId:item.id}))
+    navigation.navigate("ContactProfile")
+  }
+
   return (
     <View style={{height:150}}>
       <FlatList
@@ -15,7 +24,7 @@ const RecentAdd = ({datas}) => {
         keyExtractor={(item)=>item.id.toString()}
         renderItem={({item})=>{
         return(
-          <Pressable style={{marginRight:20,marginVertical:20,alignItems:'center'}}>
+          <Pressable onPress={()=>getCurrentContact(item)} style={{marginRight:20,marginVertical:20,alignItems:'center'}}>
             <Image source={{uri:item.photo}} style={{height:70,width:70,borderRadius:22}}/>
             <Text style={{marginTop:10,color:"#444",fontSize:14/fontScale}}>{item.firstName}</Text>
           </Pressable>
